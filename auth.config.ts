9ef5297c2135dfec67d4e06f99ec66fbb4cdba60
Daniel Bruno <daniel@hexcode.com.mx>
2024-07-18 17:59:47 -0700

    first-commit-update
    first-commit-update

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,11 @@
 import type { NextAuthConfig } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
+import { LoginSchema } from "./schemas/login.schema"
+import { db } from "./lib/db";
+import bcrypt from "bcryptjs";
+
+
+// import { db } from "./lib/db";
  
 // Notice this is only an object, not a full Auth.js instance
 export default {
@@ -7,28 +13,42 @@ export default {
     Credentials({
       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
       // e.g. domain, username, password, 2FA token, etc.
-      credentials: {
-        email: {},
-        password: {},
-      },
+      // credentials: {
+      //   email: {},
+      //   password: {},
+      // },
       authorize: async (credentials) => {
-        let user = null
- 
-        // logic to salt and hash password
-        const pwHash = saltAndHashPassword(credentials.password)
- 
-        // logic to verify if user exists
-        user = await getUserFromDb(credentials.email, pwHash)
- 
-        if (!user) {
-          // No user found, so this is their first attempt to login
-          // meaning this is also the place you could do registration
-          throw new Error("User not found.")
+        const { data, success } = LoginSchema.safeParse(credentials);
+
+        if (!success) {
+          throw new Error("Invalid credentials");
         }
- 
+        // verificar si existe el usuario en la base de datos
+        const user = await db.cat_usuarios.findUnique({
+          where: {
+            email: data.username,
+          },
+        });
+
+        if (!user || !user.password) {
+          throw new Error("No user found");
+        }
+        // verificar si la contraseña es correcta
+        const isValid = await bcrypt.compare(data.password, user.password);
+        if (!isValid) {
+          throw new Error("Incorrect password");
+        }
+
+
         // return user object with the their profile data
         return user
       },
     }),
   ],
 } satisfies NextAuthConfig;
+
+
+
+
+// export default {AASA ASASA   
+
--- components/client/loginform.tsx
+++ components/client/loginform.tsx
@@ -9,8 +9,18 @@ import { LoginSchema } from "@/schemas/login.schema"
 import {zodResolver} from '@hookform/resolvers/zod'
 import { useForm } from "react-hook-form"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
+// import { signIn } from "@/auth"
+import { signIn } from "next-auth/react"
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+import { loginAction } from "@/app/actions/login.action"
 
 export function LoginForm() {
+
+  const [error, setError] = useState<string | null>(null);
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+
   const form = useForm<z.infer<typeof LoginSchema>>({
     resolver: zodResolver(LoginSchema),
     defaultValues: {
@@ -20,17 +30,25 @@ export function LoginForm() {
     }
   });
 
-  function onSubmit(values: z.infer<typeof LoginSchema>) {
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof LoginSchema>) {
+    setError(null);
+    startTransition(async () => {
+      const response = await loginAction(values);
+      if (response.error) {
+        setError(response.error);
+      } else {
+        router.push("/private/dashboard");
+      }
+    });
   }
   return (
     <div className="flex min-h-screen w-screen w-full items-center justify-center text-gray-600 bg-gray-50">
       <div className="relative">
     
-      <div className="hidden sm:block h-56 w-56 text-indigo-300 absolute a-z-10 -left-20 -top-20">
+      <div className="hidden sm:block h-56 w-56 text-rose-300 absolute a-z-10 -left-20 -top-20">
         <svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='40' height='40' patternTransform='scale(0.6) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='none'/><path d='M11 6a5 5 0 01-5 5 5 5 0 01-5-5 5 5 0 015-5 5 5 0 015 5'  strokeWidth='1' stroke='none' fill='currentColor'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(#a)'/></svg>
         </div>
-      <div className="hidden sm:block h-28 w-28 text-indigo-300 absolute a-z-10 -right-20 -bottom-20">
+      <div className="hidden sm:block h-28 w-28 text-rose-300 absolute a-z-10 -right-20 -bottom-20">
         <svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='b' patternUnits='userSpaceOnUse' width='40' height='40' patternTransform='scale(0.5) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='none'/><path d='M11 6a5 5 0 01-5 5 5 5 0 01-5-5 5 5 0 015-5 5 5 0 015 5'  strokeWidth='1' stroke='none' fill='currentColor'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(#b)'/></svg>
       </div>
           {/* <!-- Register --> */}
@@ -38,7 +56,7 @@ export function LoginForm() {
             <div className="flex-auto p-6">
               {/* <!-- Logo --> */}
               <div className="mb-10 flex flex-shrink-0 flex-grow-0 items-center justify-center overflow-hidden">
-                <a href="#" className="flex cursor-pointer items-center gap-2 text-indigo-500 no-underline hover:text-indigo-500">
+                <a href="#" className="flex cursor-pointer items-center gap-2 text-rose-500 no-underline hover:text-rose-500">
                   <span className="flex-shrink-0 text-3xl font-black tracking-tight opacity-100">Grupo Lotificadora</span>
                 </a>
               </div>
@@ -49,7 +67,7 @@ export function LoginForm() {
                 <form onSubmit={form.handleSubmit(onSubmit)} className="mb-4" action="#" method="POST">
                   <div className="mb-4">
                     {/* <Label htmlFor="email" className="mb-2 inline-block text-xs font-medium uppercase text-gray-700">Usuario</Label> */}
-                    {/* <Input type="text" className="block w-full cursor-text appearance-none rounded-md border border-gray-400 bg--100 py-2 px-3 text-sm outline-none focus:border-indigo-500 focus:bg-white focus:text-gray-600 focus:shadow" id="username" name="email-username" placeholder="ej. dflores" autoFocus /> */}
+                    {/* <Input type="text" className="block w-full cursor-text appearance-none rounded-md border border-gray-400 bg--100 py-2 px-3 text-sm outline-none focus:border-rose-500 focus:bg-white focus:text-gray-600 focus:shadow" id="username" name="email-username" placeholder="ej. dflores" autoFocus /> */}
                     <FormField
                             control={form.control}
                             name="username"
@@ -60,7 +78,7 @@ export function LoginForm() {
                                     <Input 
                                         {...field}
                                         // disabled={isPending}
-                                         className="block w-full cursor-text appearance-none rounded-md border border-gray-400 bg--100 py-2 px-3 text-sm outline-none focus:border-indigo-500 focus:bg-white focus:text-gray-600 focus:shadow" id="username" name="email-username" placeholder="ej. dflores" autoFocus />
+                                         className="block w-full cursor-text appearance-none rounded-md border border-gray-400 bg--100 py-2 px-3 text-sm outline-none focus:border-rose-500 focus:bg-white focus:text-gray-600 focus:shadow" id="username" name="email-username" placeholder="ej. dflores" autoFocus />
                                     </FormControl>
                                     <FormMessage/>
                                 </FormItem>
@@ -73,7 +91,7 @@ export function LoginForm() {
                       {/* <Label className="mb-2 inline-block text-xs font-medium uppercase text-gray-700" htmlFor="password">Contraseña</Label> */}
                     {/* </div> */}
                     {/* <div className="relative flex w-full flex-wrap items-stretch">
-                      <Input type="password" id="password" className="relative block flex-auto cursor-text appearance-none rounded-md border border-gray-400 bg--100 py-2 px-3 text-sm outline-none focus:border-indigo-500 focus:bg-white focus:text-gray-600 focus:shadow" name="password" placeholder="············" />
+                      <Input type="password" id="password" className="relative block flex-auto cursor-text appearance-none rounded-md border border-gray-400 bg--100 py-2 px-3 text-sm outline-none focus:border-rose-500 focus:bg-white focus:text-gray-600 focus:shadow" name="password" placeholder="············" />
                     </div> */}
 
                     <FormField
@@ -90,7 +108,7 @@ export function LoginForm() {
                                         {...field}
                                         // disabled={isPending}
                                         type="password"
-                                        className="relative block flex-auto cursor-text appearance-none rounded-md border border-gray-400 bg--100 py-2 px-3 text-sm outline-none focus:border-indigo-500 focus:bg-white focus:text-gray-600 focus:shadow" name="password" placeholder="············"  />
+                                        className="relative block flex-auto cursor-text appearance-none rounded-md border border-gray-400 bg--100 py-2 px-3 text-sm outline-none focus:border-rose-500 focus:bg-white focus:text-gray-600 focus:shadow" name="password" placeholder="············"  />
                                     </div>
                                     </FormControl>
                                     <FormMessage/>
@@ -102,26 +120,26 @@ export function LoginForm() {
 
                     <div className="flex justify-between">
                       <Label className="mb-2 inline-block text-xs font-medium uppercase text-gray-700" htmlFor="password"></Label>
-                      <a href="auth-forgot-password-basic.html" className="cursor-pointer text-indigo-500 no-underline hover:text-indigo-500">
+                      <a href="auth-forgot-password-basic.html" className="cursor-pointer text-rose-500 no-underline hover:text-rose-500">
                         <small className=" ">Olvidaste tu Contraseña?</small>
                       </a>
                     </div>
                   </div>
                   <div className="mb-4">
                     <div className="block">
-                      {/* <input className="mt-1 mr-2 h-5 w-5 appearance-none rounded border border-gray-300 bg-contain bg-no-repeat align-top text-black shadow checked:bg-indigo-500 focus:border-indigo-500 focus:shadow" type="checkbox" id="remember-me" style={{        backgroundImage: 'url("data:image/svg+xml,%3csvg xmlns=\'http://www.w3.org/20/svg\' viewBox=\'0 0 20 20\'%3e%3cpath fill=\'none\' stroke=\'%23fff\' stroke-linecap=\'round\' stroke-linejoin=\'round\' strokeWidth=\'2\' d=\'M6 10l3 3l6-6\'/%3e%3c/svg%3e")'}} checked /> */}
+                      {/* <input className="mt-1 mr-2 h-5 w-5 appearance-none rounded border border-gray-300 bg-contain bg-no-repeat align-top text-black shadow checked:bg-rose-500 focus:border-rose-500 focus:shadow" type="checkbox" id="remember-me" style={{        backgroundImage: 'url("data:image/svg+xml,%3csvg xmlns=\'http://www.w3.org/20/svg\' viewBox=\'0 0 20 20\'%3e%3cpath fill=\'none\' stroke=\'%23fff\' stroke-linecap=\'round\' stroke-linejoin=\'round\' strokeWidth=\'2\' d=\'M6 10l3 3l6-6\'/%3e%3c/svg%3e")'}} checked /> */}
                       {/* <label className="inline-block" htmlFor="remember-me">Remember Me</label> */}
 
                     </div>
                   </div>
                   <div className="mb-4">
-                    <Button className="grid w-full cursor-pointer select-none rounded-md border border-indigo-500 bg-indigo-500 py-2 px-5 text-center align-middle text-sm text-white shadow hover:border-indigo-600 hover:bg-indigo-600 hover:text-white focus:border-indigo-600 focus:bg-indigo-600 focus:text-white focus:shadow-none" type="submit">Ingresar</Button>
+                    <Button className="grid w-full cursor-pointer select-none rounded-md border border-rose-500 bg-rose-500 py-2 px-5 text-center align-middle text-sm text-white shadow hover:border-rose-600 hover:bg-rose-600 hover:text-white focus:border-rose-600 focus:bg-rose-600 focus:text-white focus:shadow-none" type="submit">Ingresar</Button>
                   </div>
                 </form>
               </Form>
               {/* <p className="mb-4 text-center">
                 New on futurism?
-                <a href="#" className="cursor-pointer text-indigo-500 no-underline hover:text-indigo-500"> Create an account </a>
+                <a href="#" className="cursor-pointer text-rose-500 no-underline hover:text-rose-500"> Create an account </a>
               </p> */}
             </div>
           </div>
